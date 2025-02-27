0b377ec9aefc610ad7b9324fcdc940f63a225e79
JandyTenedora <35187380+JandyTenedora@users.noreply.github.com>
2025-02-27 20:06:19 +0000

    fix(test/pricing): fix DiscountProviderSpec, incorrect arguments

--- src/test/scala/com/adthena/shoppingbasket/pricing/DefaultDiscountProviderSpec.scala
+++ src/test/scala/com/adthena/shoppingbasket/pricing/DefaultDiscountProviderSpec.scala
@@ -13,7 +13,7 @@ class DefaultDiscountProviderSpec extends AnyFunSpec with Matchers {
       it("should apply a 10% discount to all apples") {
         val basket = Basket(List(Item("Apple", 1.00), Item("Apple", 2.00), Item("Bread", 1.50)))
         val discountedBasket = discountProvider.appleDiscounts(basket)
-        discountedBasket.items should contain allOf (Item("Apple", 0.90), Item("Apple", 1.80), Item("Bread", 1.50))
+        discountedBasket.items should contain theSameElementsAs List(Item("Apple", 0.90), Item("Apple", 1.80), Item("Bread", 1.50))
       }
 
       it("should handle an empty basket") {
@@ -25,7 +25,7 @@ class DefaultDiscountProviderSpec extends AnyFunSpec with Matchers {
       it("should not apply a discount if there are no apples") {
         val basket = Basket(List(Item("Bread", 1.50), Item("Milk", 1.00)))
         val discountedBasket = discountProvider.appleDiscounts(basket)
-        discountedBasket.items should contain allOf (Item("Bread", 1.50), Item("Milk", 1.00))
+        discountedBasket.items should contain theSameElementsAs List(Item("Bread", 1.50), Item("Milk", 1.00))
       }
     }
 
@@ -33,7 +33,7 @@ class DefaultDiscountProviderSpec extends AnyFunSpec with Matchers {
       it("should apply a half price discount to one loaf for every two tins") {
         val basket = Basket(List(Item("Soup", 1.00), Item("Soup", 1.00), Item("Bread", 1.50)))
         val discountedBasket = discountProvider.buyTwoTinsGetLoafHalfPrice(basket)
-        discountedBasket.items should contain allOf (Item("Soup", 1.00), Item("Soup", 1.00), Item("Bread", 0.75))
+        discountedBasket.items should contain theSameElementsAs List(Item("Soup", 1.00), Item("Soup", 1.00), Item("Bread", 0.75))
       }
 
       it("should handle an empty basket") {
@@ -45,13 +45,13 @@ class DefaultDiscountProviderSpec extends AnyFunSpec with Matchers {
       it("should not apply a discount if there are no tins or loaves") {
         val basket = Basket(List(Item("Milk", 1.00), Item("Cheese", 2.00)))
         val discountedBasket = discountProvider.buyTwoTinsGetLoafHalfPrice(basket)
-        discountedBasket.items should contain allOf (Item("Milk", 1.00), Item("Cheese", 2.00))
+        discountedBasket.items should contain theSameElementsAs List(Item("Milk", 1.00), Item("Cheese", 2.00))
       }
 
       it("should not apply a discount if there are less than two tins") {
         val basket = Basket(List(Item("Soup", 1.00), Item("Bread", 1.50)))
         val discountedBasket = discountProvider.buyTwoTinsGetLoafHalfPrice(basket)
-        discountedBasket.items should contain allOf (Item("Soup", 1.00), Item("Bread", 1.50))
+        discountedBasket.items should contain theSameElementsAs List(Item("Soup", 1.00), Item("Bread", 1.50))
       }
     }
 
@@ -59,7 +59,7 @@ class DefaultDiscountProviderSpec extends AnyFunSpec with Matchers {
       it("should apply a flat 5p discount to all apples") {
         val basket = Basket(List(Item("Apple", 1.00), Item("Apple", 0.10), Item("Bread", 1.50)))
         val discountedBasket = discountProvider.applesFlatDrop(basket)
-        discountedBasket.items should contain allOf (Item("Apple", 0.95), Item("Apple", 0.05), Item("Bread", 1.50))
+        discountedBasket.items should contain theSameElementsAs List(Item("Apple", 0.95), Item("Apple", 0.05), Item("Bread", 1.50))
       }
 
       it("should handle an empty basket") {
@@ -71,13 +71,13 @@ class DefaultDiscountProviderSpec extends AnyFunSpec with Matchers {
       it("should not apply a discount if there are no apples") {
         val basket = Basket(List(Item("Bread", 1.50), Item("Milk", 1.00)))
         val discountedBasket = discountProvider.applesFlatDrop(basket)
-        discountedBasket.items should contain allOf (Item("Bread", 1.50), Item("Milk", 1.00))
+        discountedBasket.items should contain theSameElementsAs List(Item("Bread", 1.50), Item("Milk", 1.00))
       }
 
       it("should not reduce the price of apples below 0") {
         val basket = Basket(List(Item("Apple", 0.03), Item("Apple", 0.02)))
         val discountedBasket = discountProvider.applesFlatDrop(basket)
-        discountedBasket.items should contain allOf (Item("Apple", 0.00), Item("Apple", 0.00))
+        discountedBasket.items should contain theSameElementsAs List(Item("Apple", 0.00), Item("Apple", 0.00))
       }
     }
   }

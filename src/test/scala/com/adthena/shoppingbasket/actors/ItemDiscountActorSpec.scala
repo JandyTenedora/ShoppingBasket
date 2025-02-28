678f86e2024164ce1c29d38157d2ee381abc3dc4
JandyTenedora <35187380+JandyTenedora@users.noreply.github.com>
2025-02-28 18:04:04 +0000

    test(actors): add Spec files for actor classes

---
+++ src/test/scala/com/adthena/shoppingbasket/actors/ItemDiscountActorSpec.scala
@@ -0,0 +1,47 @@
+package com.adthena.shoppingbasket.actors
+
+import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
+import com.adthena.shoppingbasket.models.Item
+import org.scalatest.funspec.AnyFunSpecLike
+import org.scalatest.matchers.should.Matchers
+
+class ItemDiscountActorSpec extends ScalaTestWithActorTestKit with AnyFunSpecLike with Matchers {
+
+  describe("ItemDiscountActor") {
+    it("should apply the discount function to the item") {
+      val discountFunction: Item => Item = item => item.copy(price = item.price * 0.9)
+      val itemDiscountActor = testKit.spawn(ItemDiscountActor(discountFunction))
+      val probe = testKit.createTestProbe[Item]()
+
+      val item = Item("Apples", BigDecimal(1.00))
+      itemDiscountActor ! ItemDiscountActor.ApplyItemDiscount(item, probe.ref)
+
+      val discountedItem = probe.expectMessageType[Item]
+      discountedItem shouldBe Item("Apples", BigDecimal(0.90))
+    }
+
+    it("should handle an item with zero price") {
+      val discountFunction: Item => Item = item => item.copy(price = item.price * 0.9)
+      val itemDiscountActor = testKit.spawn(ItemDiscountActor(discountFunction))
+      val probe = testKit.createTestProbe[Item]()
+
+      val item = Item("Free Sample", BigDecimal(0.00))
+      itemDiscountActor ! ItemDiscountActor.ApplyItemDiscount(item, probe.ref)
+
+      val discountedItem = probe.expectMessageType[Item]
+      discountedItem shouldBe Item("Free Sample", BigDecimal(0.00))
+    }
+
+    it("should handle an item with a negative price") {
+      val discountFunction: Item => Item = item => item.copy(price = item.price * 0.9)
+      val itemDiscountActor = testKit.spawn(ItemDiscountActor(discountFunction))
+      val probe = testKit.createTestProbe[Item]()
+
+      val item = Item("Refund", BigDecimal(-1.00))
+      itemDiscountActor ! ItemDiscountActor.ApplyItemDiscount(item, probe.ref)
+
+      val discountedItem = probe.expectMessageType[Item]
+      discountedItem shouldBe Item("Refund", BigDecimal(-0.90))
+    }
+  }
+}
---
+++ src/test/scala/com/adthena/shoppingbasket/actors/ShoppingBasketActorSpec.scala
@@ -0,0 +1,57 @@
+package com.adthena.shoppingbasket.actors
+
+import akka.actor.testkit.typed.scaladsl.{ActorTestKit, ScalaTestWithActorTestKit}
+import akka.actor.typed.ActorSystem
+import com.adthena.shoppingbasket.models.{Basket, Item}
+import com.adthena.shoppingbasket.pricing.PricingEngine
+import com.adthena.shoppingbasket.pricing.discount.DefaultDiscountProvider
+import org.scalatest.funspec.AnyFunSpecLike
+import org.scalatest.matchers.should.Matchers
+
+class ShoppingBasketActorSpec extends ScalaTestWithActorTestKit with AnyFunSpecLike with Matchers {
+  override implicit val system: ActorSystem[_] = testKit.system
+
+  describe("ShoppingBasketActor") {
+    val pricingEngine = new PricingEngine.Engine(new DefaultDiscountProvider()(system))
+    val shoppingBasketActor = testKit.spawn(ShoppingBasketActor(pricingEngine))
+
+    it("should calculate the total price of a basket with discounts applied") {
+      val probe = testKit.createTestProbe[ShoppingBasketActor.Response]()
+      val items = List(
+        Item("Apples", BigDecimal(1.00)),
+        Item("Apples", BigDecimal(1.00)),
+        Item("Soup", BigDecimal(0.65)),
+        Item("Soup", BigDecimal(0.65)),
+        Item("Bread", BigDecimal(0.80))
+      )
+
+      shoppingBasketActor ! ShoppingBasketActor.ProcessBasket(items, probe.ref)
+      val response = probe.expectMessageType[ShoppingBasketActor.BasketTotal]
+
+      response.total shouldBe "£3.50"
+    }
+
+    it("should handle an empty basket") {
+      val probe = testKit.createTestProbe[ShoppingBasketActor.Response]()
+      val items = List.empty[Item]
+
+      shoppingBasketActor ! ShoppingBasketActor.ProcessBasket(items, probe.ref)
+      val response = probe.expectMessageType[ShoppingBasketActor.BasketTotal]
+
+      response.total shouldBe "0p"
+    }
+
+    it("should calculate the total price of a basket without discounts applied") {
+      val probe = testKit.createTestProbe[ShoppingBasketActor.Response]()
+      val items = List(
+        Item("Milk", BigDecimal(1.00)),
+        Item("Cheese", BigDecimal(2.00))
+      )
+
+      shoppingBasketActor ! ShoppingBasketActor.ProcessBasket(items, probe.ref)
+      val response = probe.expectMessageType[ShoppingBasketActor.BasketTotal]
+
+      response.total shouldBe "£3.00"
+    }
+  }
+}

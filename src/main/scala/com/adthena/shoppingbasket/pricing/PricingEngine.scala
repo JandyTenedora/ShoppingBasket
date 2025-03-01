f28f9f5b16d2ce9e1f5648d581651aa927f9d03e
JandyTenedora <35187380+JandyTenedora@users.noreply.github.com>
2025-03-01 17:32:51 +0000

    feat(pricing): add requirements of discounts in applyDiscounts function

--- src/main/scala/com/adthena/shoppingbasket/pricing/PricingEngine.scala
+++ src/main/scala/com/adthena/shoppingbasket/pricing/PricingEngine.scala
@@ -11,7 +11,15 @@ object PricingEngine {
 
     // Applies all available discounts from the provider
     def applyDiscounts(basket: Basket): Basket = {
-      discountProvider.getDiscounts.foldLeft(basket)((b, discount) => discount(b))
+      val initialItems = basket.items
+      val initialItemCount = basket.items.size
+      val discountedBasket = discountProvider.getDiscounts.foldLeft(basket)((b, discount) => {
+        val newBasket = discount(b)
+        require(newBasket.items.map(_.name) == initialItems.map(_.name), "The items in the basket must remain the same after applying discounts")
+        require(newBasket.items.size == initialItemCount, "The number of items in the basket must remain the same after applying discounts")
+        newBasket
+      })
+      discountedBasket
     }
   }
 }
